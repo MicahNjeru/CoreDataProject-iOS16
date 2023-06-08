5b5f7d0bdf5811cb339081b4dc21a3caf5438434
Micah Njeru <micahnjeru1@gmail.com>
2023-06-08 09:05:30 +0300

    Initial commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Micah Jesse Njeru on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+#  Core Data Project iOS 16
+## About this project
+- This is a "learning" project in that no actual application was intended to be developed
+- It will be a repo containing my sample code when learning on how to work with Swift, SwiftUI & Core Data
+
+
