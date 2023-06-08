c6072bb036b03304c129ec1281b1abf49d785cae
Micah Njeru <micahnjeru1@gmail.com>
2023-06-08 09:26:17 +0300

    Worked on Hashable protocols

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
     var body: some View {
-        List {
-            ForEach([2, 4, 6, 8, 10], id: \.self) {
-                Text("\($0) is even")
-            }
+        List (students, id: \.self) { student in
+            Text(student.name)
         }
     }
 }
